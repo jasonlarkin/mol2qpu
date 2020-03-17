05aacc7b8ce851069652e38ea735797e69748c5b
Jason Larkin <jmlarkin@sei.cmu.edu>
2020-03-17 18:02:59 -0400

    recommiting changes

--- run_experiment_vqe_set.sh
+++ run_experiment_vqe_set.sh
@@ -4,7 +4,8 @@
 #SBATCH -t 01:00:00
 #SBATCH --ntasks-per-node 28
 
-source activate qiskit_aer
+#source activate qiskit_aer
+source activate qiskit-0.14
 
 WORK_PATH=/home/jmlarkin/mol2qpu/
 DATA_PATH=/pylon5/ci4s8dp/jmlarkin/_experiment/mol2qpu
@@ -14,8 +15,9 @@ if [ "$1" == "" ]; then
 else
     if [ "$1" == "1" ]; then    
        echo "HI, you have chosen...wisely."   
-       echo $DATA_PATH/$1.H2.vqe.out
-       python run_experiment.py --molecule='H2' --random_seed='750' --basis_set='sto3g' --algorithm='VQE' \
+       #echo $DATA_PATH/$1.H2.vqe.out
+       python run_experiment.py --molecule='H2' \ #--random_seed=750 ## throws error in numpy if u leave this in
+       --basis_set='sto3g' --algorithm='VQE' \
        --transformation_type='TransformationType.FULL' --qubitmapping_type='QubitMappingType.PARITY' --two_qubit_reduce \
        --vqe_optimizer='SPSA' --vqe_max_iter=10 --vqe_var_form='RY' --vqe_depth=3 --vqe_entangler='linear' --num_shots=1000 \
        --max_parallel_threads=10 --vqe_sim --vqe_aer --datapath=$DATA_PATH &> $DATA_PATH/$1.H2.vqe.out
@@ -28,26 +30,12 @@ else
     elif [ "$1" == "2" ]; then
        echo "HI"
        #MOL_PATH="$DATA_PATH/molecules/H2.txt"  
-       python run_experiment.py --molecule='LiH'
-       --random_seed=='750'
-       --basis_set=='sto3g'
-       --algorithm='ExactEigensolver'
-       --transformation_type=='TransformationType.FULL'   ## FIXME
-       --qubitmapping_type=='QubitMappingType.PARITY'     ## FIXME
-       --two_qubit_reduce
-       --vqe_optimizer=='SPSA' 
-       #--vqe_optimizer=='COBYLA'
-       #--vqe_optimizer=='L_BFGS_B'
-       --vqe_max_iter=='10'
-       --vqe_var_form=='RY' 
-       #--vqe_var_form=='RYRZ'
-       --vqe_depth==3
-       --vqe_entangler=='linear'
-       ##--vqe_entangler='full'                           ## FIXME
-       --num_shots==1000 
-       --max_parallel_threads=='10'
-       --vqe_sim
-       --vqe_aer 
+       python run_experiment.py --molecule='H2' \
+       #--random_seed==750' ## throws error in numpy if u leave this in
+       --basis_set=='sto3g'--algorithm='ExactEigensolver' \
+       --transformation_type=='TransformationType.FULL' --qubitmapping_type=='QubitMappingType.PARITY' \
+       --two_qubit_reduce --vqe_optimizer=='SPSA' --vqe_max_iter==10--vqe_var_form=='RY' --vqe_depth==3 \
+       --vqe_entangler=='linear' --num_shots==1000 --max_parallel_threads==10 --vqe_sim --vqe_aer \
        --datapath=="$DATA_PATH/output" &> $1.LiH.vqe.out  
     else
         echo "HI"
